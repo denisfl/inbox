00905cad88bf53bba176e9136ad889b17baaa705
Denis <denis@fedosov.me>
2026-03-22 23:08:44 +0100

    Implement URL generation methods for UnifiedStorageService to support private and public URLs (#71)

--- lib/active_storage/service/unified_storage_service.rb
+++ lib/active_storage/service/unified_storage_service.rb
@@ -114,6 +114,34 @@ module ActiveStorage
 
       private
 
+      def private_url(key, expires_in:, filename:, content_type:, disposition:, **)
+        generate_url(key, expires_in: expires_in, filename: filename, content_type: content_type, disposition: disposition)
+      end
+
+      def public_url(key, filename:, content_type: nil, disposition: :attachment, **)
+        generate_url(key, expires_in: nil, filename: filename, content_type: content_type, disposition: disposition)
+      end
+
+      def generate_url(key, expires_in:, filename:, content_type:, disposition:)
+        content_disposition = content_disposition_with(type: disposition, filename: filename)
+        verified_key_with_expiration = ActiveStorage.verifier.generate(
+          {
+            key: key,
+            disposition: content_disposition,
+            content_type: content_type,
+            service_name: name
+          },
+          expires_in: expires_in,
+          purpose: :blob_key
+        )
+
+        if url_options.blank?
+          raise ArgumentError, "Cannot generate URL for #{filename} using Unified service, please set ActiveStorage::Current.url_options."
+        end
+
+        url_helpers.rails_disk_service_url(verified_key_with_expiration, filename: filename, **url_options)
+      end
+
       def download_from_cloud(key, &block)
         if block_given?
           instrument :streaming_download, key: key do
@@ -151,6 +179,14 @@ module ActiveStorage
 
       def disk_service
         @disk_service ||= ActiveStorage::Service::DiskService.new(root: Rails.root.join("storage"))
+      end
+
+      def url_helpers
+        @url_helpers ||= Rails.application.routes.url_helpers
+      end
+
+      def url_options
+        ActiveStorage::Current.url_options
       end
     end
   end
